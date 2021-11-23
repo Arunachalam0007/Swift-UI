03846b6e49f5706a60b1bf2bea7a76300d3f7886
Arunachalam0007 <arunsha0007@gmail.com>
2021-11-23 14:42:25 +0530

    ProfileViewWithListandGrid: Created sample app.

---
+++ ProfileViewWithListAndNav/ProfileViewWithListAndNav/ProfileViewWithListAndNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewWithListAndNavApp.swift
+//  ProfileViewWithListAndNav
+//
+//  Created by ArunSha on 23/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ProfileViewWithListAndNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
